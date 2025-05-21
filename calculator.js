05c96224a319dc9d4e7d8bad39a8efa1761f578c
Zerker <84744670+Zerker-TF@users.noreply.github.com>
2025-05-21 16:22:50 -0300

    initial html style

---
+++ calculator.js
@@ -0,0 +1,15 @@
+/*
+Functions:
+add, substract, multiply, divide
+DO NOT USE EVAL
+Call function 'operation' when pressed = and have that one call one
+of the above.
+
+ buttons for each digit and operator (including =).
+ Add a “clear” button.
+ 
+ Create the functions that populate the display when you click the digit buttons.
+  You should store the content of the display (the number) in a variable for use 
+
+  Your calculator should not evaluate more than a single pair of numbers at a time.
+*/
